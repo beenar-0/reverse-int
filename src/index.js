1cb28530a7d5c4a408e75e25542047080bea335e
beenar-0 <dncastledn@gmail.com>
2022-01-05 23:49:47 +0300

    feat: add reverse-function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   if (n < 0) {
+      n = -n;
+   }
+  n = n.toString(10)
+   let temp = []
+   let len = (n.length)
+   for (let i = 0; i < len; i++) {
+      temp.unshift(n[i])
+   }
+   n = temp.join("")
+   return +n
 }
